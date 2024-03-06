3787b56c8931fcb041e079a09c57a0edf30d3670
JazminAndreaBatistaFigueroa <159494335+JazminAndreaBatistaFigueroa@users.noreply.github.com>
2024-03-06 15:01:49 -0700

    Add files via upload

---
+++ practicas/Prueba.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Prueba{ 
+   
+                 public static void main(String args[]){
+                 Scanner captura= new Scanner (System.in);
+                 System.out.println("Introduce tu nombre:");
+                 String nombre = captura.nextLine();
+                 System.out.println(nombre);
+            }
+}
