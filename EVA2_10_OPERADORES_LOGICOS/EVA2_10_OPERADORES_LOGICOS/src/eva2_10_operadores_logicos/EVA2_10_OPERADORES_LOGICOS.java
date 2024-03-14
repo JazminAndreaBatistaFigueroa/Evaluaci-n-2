de1ce8c66cf9b50b8a4b81ce9676406358282d8d
JazminAndreaBatistaFigueroa <159494335+JazminAndreaBatistaFigueroa@users.noreply.github.com>
2024-03-14 15:48:07 -0700

    Add files via upload

---
+++ EVA2_10_OPERADORES_LOGICOS/EVA2_10_OPERADORES_LOGICOS/src/eva2_10_operadores_logicos/EVA2_10_OPERADORES_LOGICOS.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_10_operadores_logicos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jazzb
+ */
+public class EVA2_10_OPERADORES_LOGICOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double moneda;
+        int adivinar;
+        Scanner captu = new Scanner(System.in);
+        adivinar = captu.nextInt();
+        
+        moneda = Math.random();
+        System.out.println("El valor de la moneda =" + moneda);
+        
+        
+        if(moneda >0.5 && adivinar == 0)
+            System.out.println("Adivinaste, es cara");
+        else
+            if(moneda > 0.5 && adivinar == 1)
+                System.out.println("Adivinaste, es cruz");
+        else
+                System.out.println("Perdiste");
+        
+        
+    }
+    
+}
